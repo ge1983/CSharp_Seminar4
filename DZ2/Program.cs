98a9b9f62c8ee4d2c166422b03f6261e19e921b9
Admin <ge1983@yandex.ru>
2023-03-07 17:58:59 +1000

    Добавлено задача 2 для ДЗ

---
+++ DZ2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int sumOfDigits (int number)
+{
+    int result = 0;
+    while (number>0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+Console.Write("Ввелите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Сумма цифр числа {Number} равна {sumOfDigits(Number)}");
