08b5e8af77cc805a8fb12d5dd41668ee17afc63b
Admin <ge1983@yandex.ru>
2023-03-07 17:46:26 +1000

    Добавлено задача 1 для ДЗ

---
+++ DZ1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int exponent (int number, int degree)
+{
+    int result = 1;    
+    for (int i=1; i<=degree; i++)
+    {
+        result = result * number;
+    }
+
+    return result;
+}
+
+
+Console.Write("Ввелите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ввелите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Число {A} в степени {B} равно {exponent(A, B)}");
